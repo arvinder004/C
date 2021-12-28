977ede91a898026982f617dc4b15187c4517ea34
Arvinder Singh Dhoul <asdhoul004@gmail.com>
2021-12-28 21:20:22 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -3,7 +3,7 @@
 #include<stdio.h>
 int main(){
 	int a,b=1;
-	printf("ENTER THE NUMBER TO FIND FACTORIAL:");
+	printf("ENTER THE NUMBER TO FIND FACTORIAL:");    //asking the number to find factorial
 	scanf("%d",&a);
 	for (int i = 1;i<=a;i++){
 		b*=i;
