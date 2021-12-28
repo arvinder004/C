827fc85ff38a18eed626a5c8ec4b9f948f76cea2
Arvinder Singh Dhoul <asdhoul004@gmail.com>
2021-12-28 21:19:06 +0530

    Update biggest_value.cpp

--- biggest_value.cpp
+++ biggest_value.cpp
@@ -3,17 +3,17 @@
 #include<stdio.h>
 int main(){
 	int a,b,c;
-	printf("ENTER 3 NUMBER TO FIND BIGGEST VALUE\n:");
+	printf("ENTER 3 NUMBER TO FIND BIGGEST VALUE\n:");    //asking input as three numbers 
 	scanf("%d,%d,%d",a,b,c);
 	if (a>b && a>c){
-		printf("%d is bigger than %d and %d",a,b,c);
+		printf("%d is bigger than %d and %d",a,b,c);  //checks first number with other 2
 	}
 	
 	else if (b>a && b>c){
-		printf("%d is bigger than %d and %d",b,a,c);
+		printf("%d is bigger than %d and %d",b,a,c);  //checks second number with other 2
 	}
 	
 	else{
-		printf("%d is bigger than %d and %d",c,a,b);
+		printf("%d is bigger than %d and %d",c,a,b);  //checks third number with other 2
 	}
 }
