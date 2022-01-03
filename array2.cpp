ceac23772b6e76b9faa2133a05d1869cef44d348
Arvinder Singh Dhoul <asdhoul004@gmail.com>
2022-01-03 13:40:59 +0530

    Create array2.cpp

---
+++ array2.cpp
@@ -0,0 +1,18 @@
+///printing an array in reverse
+
+#include<stdio.h>
+int main(){
+
+	int arr[50],n;
+	printf("enter the number of elements less than 50\n");
+	scanf("%d",&n);
+	printf("enter your elements\n");
+	for(int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	printf("your array is\n");
+	for(int i=n-1;i>=0;i--){
+		printf("%d ",arr[i]);
+	}
+	return 0;
+}
