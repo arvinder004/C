d74fae4545d9501fb50f1b2bacc289976ebe988e
Arvinder Singh Dhoul <asdhoul004@gmail.com>
2022-01-05 13:04:52 +0530

    Update matrixadd.cpp

--- matrixadd.cpp
+++ matrixadd.cpp
@@ -3,9 +3,9 @@
 #include<stdio.h>
 int main(){
 	int arr1[2][2] = {{1,2},{3,4}},arr2[2][2] = {{5,6},{7,8}},arr3[2][2] = {{0,0},{0,0}};
-	for(int i=0;i<2;i++){
-		for(int j=0;j<2;j++){
-			arr3[i][j]=arr1[i][j]+arr2[i][j];
+	for(int i=0;i<2;i++){           //rows
+		for(int j=0;j<2;j++){             //coloumns
+			arr3[i][j]=arr1[i][j]+arr2[i][j];      //adding arr1 and arr2,storing value in arr3
 			printf("%d ",arr3[i][j]);
 		}
 		printf("\n");
