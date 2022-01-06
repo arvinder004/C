1626e8404d80c6cfa3331b4b7d764014eae0f3b3
Arvinder Singh Dhoul <asdhoul004@gmail.com>
2022-01-06 21:18:52 +0530

    Update array1.cpp

--- array1.cpp
+++ array1.cpp
@@ -3,10 +3,10 @@
 #include<stdio.h>
 int main(){
 
-	int arr[50],n;
-	printf("entr the number of elements less than 50\n");
+	int arr[50],n;             //assinging max 50 values to the array
+	printf("entr the number of elements less than 50\n");            //taking input from user for number of values
 	scanf("%d",&n);
-	printf("enter your elements\n");
+	printf("enter your elements\n");         //taking input from user for values
 	for(int i=0;i<n;i++){
 		scanf("%d",&arr[i]);
 	}
