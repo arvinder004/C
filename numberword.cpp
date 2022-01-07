26f0dd6c96e9a2e3a78616da99e1b60314cc92fc
Arvinder Singh Dhoul <asdhoul004@gmail.com>
2022-01-07 23:11:08 +0530

    Create numberword.cpp

---
+++ numberword.cpp
@@ -0,0 +1,39 @@
+//PROGRAM TO CONVERT NUMBER BETWEEN 1-10 TO WORDS
+
+#include<stdio.h>
+int main(){
+	int num;
+	printf("ENTER ANY NUMBER BETWEEN 1-10 \n");
+	scanf("%d",&num);
+	if(num==1){
+		printf("ONE");
+	}
+	if(num==2){
+		printf("TWO");
+	}
+	if(num==3){
+		printf("THREE");
+	}
+	if(num==4){
+		printf("FOUR");
+	}
+	if(num==5){
+		printf("FIVE");
+	}
+	if(num==6){
+		printf("SIX");
+	}
+	if(num==7){
+		printf("SEVEN");
+	}
+	if(num==8){
+		printf("EIGHT");
+	}
+	if(num==9){
+		printf("NINE");
+	}
+	if(num==10){
+		printf("TEN");
+	}
+	return 0;
+}
