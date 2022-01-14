d80ab8ac577e24d3620351684e6a63ec48b2bbb4
Arvinder Singh Dhoul <asdhoul004@gmail.com>
2022-01-14 13:33:41 +0530

    Update area.cpp

--- area.cpp
+++ area.cpp
@@ -5,14 +5,15 @@ int area(int x,int y);
 main()
 {
 int a,b;
-printf("enter length and breadth of rectangle to find area \n");
+printf("enter length and breadth of rectangle to find area \n");   //taking input from user
+
 scanf("%d %d",&a,&b);
-area(a,b);
+area(a,b);      //function call
 //printf("%d",a);
 }
 
-int area(int x,int y)
-{ 
+int area(int x,int y)  //defining a function 
+{  
 x*=y;
 printf("%d",x);
 
